370ce657f581550095a7e26380e011d0279fbc60
Dylan Meeus <meeusdylan@protonmail.com>
2020-04-04 18:51:26 +0200

    Add try-except logic to web-element actions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
 import sys
+import json
 from webbot import Browser
-
 
 url = "https://read.amazon.com/kp/notebook"
 
@@ -22,8 +23,12 @@ def extractBooks(web):
     return books
 
 def extractHighlights(web, book):
-    web.click(book)
-    elements = web.find_elements(id='highlight')
+    try:
+        web.click(book)
+        elements = web.find_elements(id='highlight')
+    except:
+        print("could not read " + book)
+        return
     highlights = []
     for e in elements:
         highlights.append(e.text)
@@ -35,8 +40,18 @@ if __name__ == '__main__':
     web = Browser(showWindow=True) # easier debugging
     login(web)
     books = extractBooks(web)
-    qs = extractHighlights(web, books[2])
-    for q in qs:
-        print(q)
+    book_highlights = {}
+    for book in books:
+        print("extracting highlights for " + book)
+        hs = extractHighlights(web, book)
+        book_highlights[book] = hs
+    print("writing to json file")
+    js = json.dumps(book_highlights)
+    f = open("highlights.json", 'w')
+    f.write(js)
+    f.close()
+    print("done")
+
+
     
 
